3fca3d9fb062d4f34b3c85304fac55a7036494ee
Racconeee <raccon6614@gmail.com>
2023-09-06 20:06:39 +0900

    first

---
+++ backend/DoitDoit/src/main/java/project/DoitDoit/DTO/Main_BoardDTO.java
@@ -0,0 +1,14 @@
+package project.DoitDoit.DTO;
+
+import java.util.Date;
+import java.util.List;
+
+public class Main_BoardDTO {
+
+    private Long board_id;
+    private String title;
+    private String Content;
+    private Long member_id;
+    private Boolean favorites;
+    private Date current_date;
+}
---
+++ backend/DoitDoit/src/test/java/project/DoitDoit/DoitDoitApplicationTests.java
@@ -0,0 +1,13 @@
+package project.DoitDoit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoitDoitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
