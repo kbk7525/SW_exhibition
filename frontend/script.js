475e1a51f232c63e1e6d71ddb38ca55165930a0f
dlrjstkd1123 <105963236+dlrjstkd1123@users.noreply.github.com>
2023-10-25 19:05:53 +0900

    달력까지 완성

--- frontend/script.js
+++ frontend/script.js
@@ -94,14 +94,14 @@ function submitForm(event) {
 
     // 날짜 값을 년-월-일 형식으로 가공
     const selectedDate = new Date(todaylistDate);
-    const formattedDate = selectedDate.getFullYear() + '-' + 
-                         String(selectedDate.getMonth() + 1).padStart(2, '0') + '-' + 
-                         String(selectedDate.getDate()).padStart(2, '0');
+    const formattedDate = selectedDate.getFullYear() + '-' +
+        String(selectedDate.getMonth() + 1).padStart(2, '0') + '-' +
+        String(selectedDate.getDate()).padStart(2, '0');
 
     const data = {
         title: taskName,
         content: todaylistWrite,
-        today: formattedDate,
+        dDay: formattedDate,
     };
     console.log(data);
     const serverUrl = "http://localhost:8082/mainpage/save";
@@ -113,22 +113,22 @@ function submitForm(event) {
         },
         body: JSON.stringify(data),
     })
-    .then((responseData) => {
-        if (responseData.status === 200) {
-            alert("작업 추가 성공");
-            taskNameInput.value = "";
-            todaylistWriteInput.value = "";
-            todaylistDateInput.value = "";
-            const _mod_todaylist = document.querySelector(".todaylist")
-            _mod_todaylist.classList.remove("active");
-        } else {
-            alert("작업 추가 실패");
-        }
-    })
-    .catch((error) => {
-        console.error("에러 발생:", error);
-        alert("작업 추가 요청 중 오류가 발생했습니다.");
-    });
+        .then((responseData) => {
+            if (responseData.status === 200) {
+                alert("작업 추가 성공");
+                taskNameInput.value = "";
+                todaylistWriteInput.value = "";
+                todaylistDateInput.value = "";
+                const _mod_todaylist = document.querySelector(".todaylist")
+                _mod_todaylist.classList.remove("active");
+            } else {
+                alert("작업 추가 실패");
+            }
+        })
+        .catch((error) => {
+            console.error("에러 발생:", error);
+            alert("작업 추가 요청 중 오류가 발생했습니다.");
+        });
 }
 
 
@@ -175,7 +175,7 @@ function submitForm2(event) {
                 DrawingListDateFocus.value == "";
                 const mod_drawing_list = document.querySelector(".mod_drawing_list");
                 mod_drawing_list.classList.remove("active");
-                
+
 
             } else {
                 alert("작업 추가 실패");
@@ -191,22 +191,7 @@ function submitForm2(event) {
 
 
 
-/* 서버에 데이터 요청*/
-const serverUrl = "http://localhost:8082/mainpage";
-fetch(serverUrl)
-    .then(response => {
-        if (!response.ok) {
-            throw new Error("서버 응답 오류");
-        }
-        return response.json(); // JSON 형식의 응답 데이터를 파싱
-    })
-    .then(data => {
-        // 서버로부터 받은 데이터(data)를 처리합니다.
-        console.log(data);
-    })
-    .catch(error => {
-        console.error("에러 발생:", error);
-    });
+
 
 
 
@@ -231,13 +216,44 @@ const formattedDate = now.toLocaleDateString(undefined, options);
 const calenderHead = document.querySelector(".mod_calender_head");
 calenderHead.textContent = formattedDate;
 
+function openModalForDate(date) {
+    // 모달 창 열기
+    const modal = document.querySelector(".calender_select");
+    modal.style.display = "block";
+
+    // 모달 창 내용을 초기화
+    const modalSection = document.querySelector(".mod_calender_select_section");
+    modalSection.innerHTML = "";
+
+    // 날짜를 서버로 보내 데이터를 가져오는 요청을 보냅니다.
+    fetch(`http://localhost:8082/mainpage/save${date}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        // 데이터를 모달 창에 추가
+        data.forEach(item => {
+            const todoItem = document.createElement("div");
+            todoItem.classList.add("todo-item");
+            todoItem.innerHTML = `
+                <h3>${item.title}</h3>
+                <p>${item.content}</p>
+                
+            `;
+            modalSection.appendChild(todoItem);
+        });
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+}
 /*달력 몸통 만든 함수 */
 function createCalendar(year, month) {
     const calenderBody = document.querySelector(".calender_body");
     calenderBody.innerHTML = "";
-
-
-
 
     const date = new Date(year, month - 1, 1);
     const lastDate = new Date(year, month, 0);
@@ -251,16 +267,20 @@ function createCalendar(year, month) {
             const cell = document.createElement("td");
             if (i === 0 && j < date.getDay()) {
                 cell.textContent = ".";
-
             }
             else if (nowDate <= lastDate.getDate()) {
                 cell.textContent = nowDate;
+
+                // 클릭 이벤트를 추가하여 모달 열기
+                cell.addEventListener("click", function () {
+                    openModalForDate(`${year}-${month}-${nowDate}`);
+                });
+
                 nowDate++;
             }
             else {
                 cell.textContent = nextmonthdate;
                 nextmonthdate++;
-
             }
             row.appendChild(cell);
         }
@@ -269,9 +289,6 @@ function createCalendar(year, month) {
         if (nowDate > lastDate.getDate()) {
             break;
         }
-
-
-
     }
 }
 
@@ -345,3 +362,101 @@ function close_mod_drawing_list() {
     const mod_drawing_list = document.querySelector(".mod_drawing_list");
     mod_drawing_list.classList.remove("active");
 }
+
+
+function displayTodoList() {
+    const data1 = [
+        {
+            "boardId": 1,
+            "title": "hi",
+            "content": "1234",
+            "today": "2023-09-26"
+        },
+        {
+            "boardId": 2,
+            "title": "hi",
+            "content": "1234",
+            "today": "2023-10-24"
+        },
+        {
+            "boardId": 3,
+            "title": "hi",
+            "content": "12dsdf",
+            "today": "2023-10-24"
+        },
+        {
+            "boardId": 4,
+            "title": "hi",
+            "content": "1234",
+            "today": "2023-10-18"
+        }
+    ]
+
+    const todoList = document.getElementById("todoList");
+    const todayDate = new Date();
+    const todoItemDate = document.createElement("div")
+    todoItemDate.innerHTML +=`
+                <p>${todayDate.toISOString().split('T')[0]}</p>
+                
+            `;;
+    todoItemDate.classList.add("todoItemDate");
+    todoList.appendChild(todoItemDate);
+    data1.forEach(item => {
+        const todoItem = document.createElement("div");
+        const todoItemList = document.createElement("div");
+        
+        todoItemList.classList.add("todoItemList");
+        todoItem.classList.add("todo-item");
+
+        // 새로운 <i> 요소를 생성하고 속성을 설정합니다.
+        const xMark = document.createElement("i");
+        xMark.classList.add("fa-solid", "fa-xmark");
+
+        // xMark를 클릭했을 때 해당 todoItem을 제거하는 이벤트 리스너를 추가합니다.
+        xMark.addEventListener("click", function () {
+            // 클라이언트에서 서버로 삭제 요청을 보냅니다.
+            const boardId = item.boardId; // 삭제할 항목의 boardId 가져오기
+            fetch(`http://localhost:8082/mainpage/save${boardId}`, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(response => {
+                    if (response.status === 200) {
+                        // 서버에서 삭제가 성공한 경우 클라이언트에서도 삭제
+                        todoList.removeChild(todoItem);
+                    }
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                });
+        });
+
+        // 현재 날짜를 가져옵니다.
+        const todayDate = new Date();
+        // item.today와 현재 날짜(todayDate)를 비교합니다.
+        const itemDate = new Date(item.today);
+
+        if (
+            todayDate.getFullYear() === itemDate.getFullYear() &&
+            todayDate.getMonth() === itemDate.getMonth() &&
+            todayDate.getDate() === itemDate.getDate()
+        ) {
+            // 만약 item.today가 오늘 날짜와 일치하면 아래 내용을 출력합니다.
+            todoItemList.innerHTML += `
+                
+                <h3>${item.title}</h3>
+                <p>${item.content}</p>
+                
+            `;
+            
+            
+            
+            todoList.appendChild(todoItem);
+            todoItem.appendChild(todoItemList);
+            todoItem.appendChild(xMark); // todoItem을 todoList에 추가합니다.
+        }
+    });
+}
+
